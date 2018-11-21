ff2d343fa63cc448e7d6c119247443e4bf258876
haitv282 <haitv282@gmail.com>
2018-11-21 09:31:01 +0700

    Init module template

---
+++ Helper/Data.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_SameOrderNumber
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\SameOrderNumber\Helper;
+
+use Mageplaza\Core\Helper\AbstractData;
+
+/**
+ * Class Data
+ * @package Mageplaza\SameOrderNumber\Helper
+ */
+class Data extends AbstractData
+{
+    const CONFIG_MODULE_PATH = 'sameordernumber';
+}
